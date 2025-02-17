31f073190d5224c20deb5fe06f5d52ff93738177
TikiTop <cpttiki@gmail.com>
2025-02-17 07:09:33 -0500

    Cleanup refactor
    xfade from slash to run animations
    added jump function
    added idle function

--- player/player.gd
+++ player/player.gd
@@ -24,24 +24,14 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	frame_camera_rotation()
+	var direction := get_movement_direction()
+	rig.update_animation_tree(direction)
+	handle_jump_physics_frame()
+	handle_idle_physics_frame(delta, direction)
+	handle_slashing_physics_frame(delta)
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	var direction := get_movement_direction()
-	rig.update_animation_tree(direction)
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-		look_toward_direction(direction, delta)
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-	handle_slashing_physics_frame(delta)
 	move_and_slide()
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -90,9 +80,26 @@ func slash_attack() -> void:
 	if _attack_direction.is_zero_approx():
 		_attack_direction = rig.global_basis * Vector3(0,0,1) #0,0,1 is the facing direction of the player
 
+func handle_idle_physics_frame(delta: float, direction:Vector3) -> void:
+	if not rig.is_idle():
+		return
+	#if there is a direction of movement - point that way and move
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+		look_toward_direction(direction, delta)
+	else: #otherwise slow down
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
 func handle_slashing_physics_frame(delta: float) -> void:
 	if not rig.is_slashing():
 		return
 	velocity.x = _attack_direction.x * attack_move_speed
 	velocity.z = _attack_direction.z * attack_move_speed
 	look_toward_direction(_attack_direction, delta)
+
+func handle_jump_physics_frame() -> void:
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	pass
