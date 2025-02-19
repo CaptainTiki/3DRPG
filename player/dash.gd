4119981221d8bf41efa2d6f61efabac9e84bd576
CptTiki <CptTiki@gmail.com>
2025-02-19 11:45:21 -0500

    Dash Particles
    Added gpu particles to dash and set to spawn only when dashing

--- player/dash.gd
+++ player/dash.gd
@@ -4,6 +4,7 @@ extends Node3D
 @export var speed_multiplier : float = 3.0
 
 @onready var timer: Timer = $Timer
+@onready var gpu_particles_3d: GPUParticles3D = $GPUParticles3D
 
 var direction : Vector3 = Vector3.ZERO
 var dash_duration : float = 0.1
@@ -17,6 +18,7 @@ func _unhandled_input(event: InputEvent) -> void:
 		
 		if not direction.is_zero_approx(): #if direction is approx zero - then we aren't moving
 			player.rig.travel("Dash")
+			gpu_particles_3d.emitting = true
 			timer.start(1.0)
 			time_remaining = dash_duration
 
@@ -26,7 +28,5 @@ func _physics_process(delta: float) -> void:
 	player.velocity = direction * player.SPEED * speed_multiplier
 	time_remaining -= delta
 	if time_remaining <= 0:
-		pass
 		direction = Vector3.ZERO
-	else:
-		pass
+		gpu_particles_3d.emitting = false
