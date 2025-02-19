b584dc8aee3454a23b5b3ef69dfbeea04b510f2d
CptTiki <CptTiki@gmail.com>
2025-02-19 14:06:17 -0500

    Ability func
    added percentile calc for ability scores
    added default get function
    added new abilityes for speed, agility, endurance and strength

--- resources/character_stats.gd
+++ resources/character_stats.gd
@@ -11,11 +11,26 @@ class Ability:
 	func _init(min:float, max:float) -> void:
 		min_modifier = min
 		max_modifier = max
+	
+	func percentile_lerp(min_boundary: float, max_boundary: float)-> float:
+		return lerp(min_boundary, max_boundary, ability_score / 100.0)
+	
+	func get_modifer() -> float:
+		return percentile_lerp(min_modifier, max_modifier)
+
+
 
 var level : int = 1
 var xp : int = 1
 
-var strength
-var speed = Ability.new(3.0, 7.0)
-var endurance
-var agility
+#damage bonus on attack
+var strength := Ability.new(2.0, 12.0)
+#movement speed in m/s
+var speed := Ability.new(3.0, 7.0)
+#extra hitpoints per level
+var endurance := Ability.new(5.0, 25.0)
+#crit chance
+var agility := Ability.new(0.05, 0.25)
+
+func get_base_speed() -> float:
+	return speed.get_modifier()
