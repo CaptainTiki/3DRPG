971ac07f606c428f6eb054c346adc3e1a46e9e49
CptTiki <CptTiki@gmail.com>
2025-02-19 13:31:41 -0500

    Ability Class
    set up ability class variables
    stubbed out init func
    clamped ability variables to 0 to 100

--- resources/character_stats.gd
+++ resources/character_stats.gd
@@ -1,10 +1,21 @@
 extends Resource
 class_name CharacterStats
 
+class Ability:
+	var min_modifier: float
+	var max_modifier: float
+	
+	var ability_score: int = 25:
+		set(value): ability_score = clamp(value, 0, 100)
+	
+	func _init(min:float, max:float) -> void:
+		min_modifier = min
+		max_modifier = max
+
 var level : int = 1
 var xp : int = 1
 
 var strength
-var speed
+var speed = Ability.new(3.0, 7.0)
 var endurance
 var agility
