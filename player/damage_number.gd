569e3eb80e77a1a45ffa5493aa019d1e6bfdba2f
TikiTop <cpttiki@gmail.com>
2025-02-20 14:43:54 -0500

    Damage Number node
    damage numbers setup

---
+++ player/damage_number.gd
@@ -0,0 +1,8 @@
+extends Node3D
+
+@onready var label_3d: Label3D = $Label3D
+
+func setup(damage: int, color: Color, position_in: Vector3) -> void:
+	label_3d.text = str(damage)
+	label_3d.modulate = color
+	global_position = position_in
