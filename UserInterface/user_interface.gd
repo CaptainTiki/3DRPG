762547744e1380e19a0fb859437beeb07035187d
CptTiki <CptTiki@gmail.com>
2025-02-21 08:59:52 -0500

    Inventory stub
    added inventory screen
    setup functions to open and close in UI node

--- UserInterface/user_interface.gd
+++ UserInterface/user_interface.gd
@@ -3,10 +3,32 @@ extends Control
 @onready var level_label: Label = %LevelLabel
 @onready var health_bar: TextureProgressBar = %HealthBar
 @onready var xp_bar: TextureProgressBar = %XPBar
+@onready var health_label: Label = %HealthLabel
+@onready var inventory: Control = $Inventory
 
 @export var player : Player
+
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("Open_Menu"):
+		if not inventory.visible:
+			open_menu()
+		else:
+			close_menu()
 
 func update_stats_display() -> void:
 	level_label.text = str(player.stats.level)
 	xp_bar.max_value = player.stats.percentage_level_up_boundary()
 	xp_bar.value = player.stats.xp
+
+func update_health() -> void:
+	health_bar.max_value = player.health_component.max_health
+	health_bar.value = player.health_component.current_health
+	health_label.text = player.health_component.get_health_label_string()
+
+func open_menu() -> void:
+	inventory.visible = true
+	pass
+
+func close_menu() -> void:
+	inventory.visible = false
+	pass
--- player/health_component.gd
+++ player/health_component.gd
@@ -27,4 +27,6 @@ func take_damage(damage_in: float, is_critical: bool) -> void:
 	else:
 		VfxManager.spawn_damage_number(damage, Color.WHITE, body.global_position )
 	current_health -= damage
-	
+
+func get_health_label_string() -> String:
+	return "%s/%s" % [current_health, max_health]
