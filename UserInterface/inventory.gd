c2d4e298b220bfef59fabd41df52b817ca010fa1
TikiTop <cpttiki@gmail.com>
2025-03-04 13:23:53 -0500

    Weapon swap
    added function to swap weapon in equipment
    added 3 new weapons to level in chests

--- UserInterface/inventory.gd
+++ UserInterface/inventory.gd
@@ -9,9 +9,17 @@ class_name Inventory
 @onready var speed_value: Label = %SpeedValue
 @onready var attack_value: Label = %AttackValue
 @onready var item_grid: GridContainer = %ItemGrid
+@onready var gold_label: Label = %GoldLabel
+@onready var weapon_slot: CenterContainer = %WeaponSlot
+@onready var sheild_slot: CenterContainer = %SheildSlot
+@onready var armor_slot: CenterContainer = %ArmorSlot
 
 @onready var player : Player = get_parent().player
-
+@onready var gold : int = 0:
+	set(value):
+		gold = value
+		gold_label.text = str(gold) + "g"
+		
 func _ready() -> void:
 	update_stats()
 
@@ -35,5 +43,24 @@ func _on_texture_button_pressed() -> void:
 	get_parent().close_menu()
 
 func add_item(icon: ItemIcon) -> void:
+	for connection in icon.interact.get_connections():
+		icon.interact.disconnect(connection["callable"])
 	icon.get_parent().remove_child(icon)
 	item_grid.add_child(icon)
+	icon.interact.connect(interact)
+
+func add_currency(currency_in: int) -> void:
+	print(gold)
+	gold += currency_in
+
+func equip_item(item: ItemIcon, item_slot: CenterContainer) -> void:
+	for child in item_slot.get_children():
+		add_item(child)
+	item.get_parent().remove_child(item)
+	item_slot.add_child(item)
+
+func interact(item: ItemIcon) -> void:
+	if item is WeaponIcon:
+		equip_item(item, weapon_slot)
+	
+	update_gear_stats()
